9d508d61a1e3ca9eb0c3bc236398e7a9af18befa
Sebastian-Balint <sebastian.balint02@gmail.com>
2024-03-13 09:08:34 +0000

    add: jsnack3

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,3 +1,23 @@
 // - Crea un array vuoto.
 //   Chiedi per 6 volte all’utente di inserire un numero,
 //   se è dispari inseriscilo nell’array.
+
+// quantita' di numeri che voglio
+const howManyNumbers = 6;
+// creo un'array vuota
+const numbers = [];
+
+// uso il ciclo for per chiedre 6 numeri all'utente
+for (let i = 0; i < howManyNumbers; i++) {
+  const userNumber = parseInt(prompt("Inserisci un numero"));
+
+  // controllo se è pari e non lo inserisco
+  if (userNumber % 2 === 0) {
+    numbers.push();
+  } else {
+    // se è dispari lo inserisco all'interno dell'array
+    numbers.push(userNumber);
+  }
+}
+// risultato
+console.log(numbers);
