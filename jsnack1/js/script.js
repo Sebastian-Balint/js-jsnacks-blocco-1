2483f0b656e678f6ca6bdb75ed6409c2c9102e3c
Sebastian-Balint <sebastian.balint02@gmail.com>
2024-03-13 09:04:59 +0000

    add: jsnack1 and jsnack2

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,2 +1,20 @@
 // - Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare
 // duplicati (inserire il numero nell'array solo se non è già presente).
+
+// quantita' di numeri che voglio
+const howManyNumbers = 5;
+// creo un'array vuota
+const numbers = [];
+
+// uso il ciclo for per chiedre 5 numeri all'utente
+for (let i = 0; i < howManyNumbers; i++) {
+  const userNumber = parseInt(prompt("Inserisci un numero"));
+
+  // controllo se non sono già stati inserirti
+  if (!numbers.includes(userNumber)) {
+    // inserisco all'interno dell'array
+    numbers.push(userNumber);
+  }
+}
+// risultato
+console.log(numbers);
--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,2 +1,18 @@
 // - Il software deve chiedere per 5 volte all’utente di inserire un numero.
 //   Il programma stampa la somma di tutti i numeri inseriti.
+
+// quantita' di numeri che voglio
+const howManyNumbers = 5;
+// creo un'array vuota
+const numbers = [];
+
+// dichiaro la variabile somma
+let sum = 0;
+// uso il ciclo for per chiedre 5 numeri all'utente
+for (let i = 0; i < howManyNumbers; i++) {
+  const userNumber = parseInt(prompt("Inserisci un numero"));
+  // sommo i numeri che mi sono stati dati
+  sum += userNumber;
+}
+// risultato
+alert("La somma dei numeri forniti è " + sum);
